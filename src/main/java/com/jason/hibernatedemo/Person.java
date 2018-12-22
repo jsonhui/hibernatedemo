e54b388334aa8a45729669af48ad281273014c75
Jason <320175912@qq.com>
2018-12-22 18:33:35 +0800

    初始导入。

---
+++ src/main/java/com/jason/hibernatedemo/Person.java
@@ -0,0 +1,32 @@
+package com.jason.hibernatedemo;
+
+public class Person {
+	private Integer id;
+	private String name;
+	private String money;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMoney() {
+		return money;
+	}
+
+	public void setMoney(String money) {
+		this.money = money;
+	}
+
+}
